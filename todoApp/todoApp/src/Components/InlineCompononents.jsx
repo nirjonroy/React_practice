efbc46d9f2a9b24d784a9e7aab93ab239517bf55
nirjonroy <roynirjon18@gmail.com>
2024-12-29 18:57:20 +0600

    external css

--- todoApp/todoApp/src/Components/InlineCompononents.jsx
+++ todoApp/todoApp/src/Components/InlineCompononents.jsx
@@ -1,9 +1,9 @@
-
+import "../css/style.css";
 const header = {color: "blue", fontSize: "140px"}
 export default function InlineCompononents(){
     return (
         <div >
-            <h1 style={header}>Inline Components</h1>
+            <h1 className="header">Inline Components</h1>
         </div>
     )
 }
