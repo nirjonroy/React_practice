871923616c86f476a009df40e251578f06a54c76
nirjonroy <roynirjon18@gmail.com>
2025-01-02 21:46:51 +0600

    Separating JSX Into Components

--- todoApp/todoApp/src/Components/Todo.jsx
+++ todoApp/todoApp/src/Components/Todo.jsx
@@ -1,28 +1,16 @@
 import { useState } from "react";
-import Todoitem from "./Todoitem";
+
+import Form from "./Form";
+import TodoList from "./TodoList";
 
 export default function Todo() {
-    const [todo, setTodo] = useState("");
+    
     const [todos, setTodos] = useState([]);
-    function handleSubmit(e){
-        e.preventDefault();
-        setTodos([...todos, todo]);
-        setTodo("");
-    }
+   
     return (
         <div>
-            <form action="" onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    onChange={(e) => setTodo(e.target.value)} 
-                    value={todo}
-                />
-                <button type="submit">Add</button>
-            </form>
-            {todos.map(item=>(
-                
-                <Todoitem key={item} item={item}/>
-            ))}
+            <Form todos={todos} setTodos={setTodos} />
+            <TodoList todos={todos} />
         </div>
     );
 }
---
+++ todoApp/todoApp/src/Components/TodoList.jsx
@@ -0,0 +1,12 @@
+import Todoitem from "./Todoitem";
+export default function TodoList({todos}){
+    return (
+        <div>
+            {todos.map(item=>(
+                
+                <Todoitem key={item} item={item}/>
+            ))}
+        </div>
+    )
+
+}
