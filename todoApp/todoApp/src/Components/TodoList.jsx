ba9a3986c3e54ce71c805838188c050df701b7d8
nirjonroy <roynirjon18@gmail.com>
2025-01-06 23:37:07 +0600

    Sorting Items In A Todo List

--- todoApp/todoApp/src/Components/TodoList.jsx
+++ todoApp/todoApp/src/Components/TodoList.jsx
@@ -1,9 +1,10 @@
 import Todoitem from "./Todoitem";
 import style from "./todolist.module.css";
 export default function TodoList({todos, setTodos}){
+    const sortedTodos = todos.slice().sort((a,b)=>Number(a.done - b.done));
     return (
         <div className={style.list}>
-            {todos.map(item=>(
+            {sortedTodos.map(item=>(
                 
                 <Todoitem key={item.name} item={item} todos={todos} setTodos={setTodos}/>
             ))}
